b142c2118c3d9a9d06a02cc8b17acb24165b4de4
Dwayne Patel <dwaynepatel@hotmail.com>
2017-08-16 15:45:36 +0100

    make it better

--- src/app/items/shared/item.ts
+++ src/app/items/shared/item.ts
@@ -1,6 +1,9 @@
 export class Item {
   $key: string;
   title: string;
+  weight: string;
+  name: string;
+  date: string;
   body: string;
   active: boolean = true;
   timeStamp: Date = new Date();
